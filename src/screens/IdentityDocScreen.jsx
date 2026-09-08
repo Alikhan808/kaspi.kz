4deeba46cd2f43f8f2c39b9bfa08a40f5fceccf0
Alikhan808 <aa.dauletuly@gmail.com>
2026-09-09 01:23:36 +0500

    Change image file extensions from .png to .jpg

--- src/screens/IdentityDocScreen.jsx
+++ src/screens/IdentityDocScreen.jsx
@@ -272,11 +272,10 @@ export default function IdentityDocScreen({ onBack }) {
         {tab === 'document' ? (
           <>
             <button className="id-btn id-btn--primary" onClick={() => fileInputRef.current?.click()}>
-              <img src="/icons/documentt.png" alt="предъявить" />
+              <img src="/icons/documentt.jpg" alt="предъявить" />
             </button>
             <button className="id-btn id-btn--secondary" onClick={() => fileInputRef.current?.click()}>
-              <img src="/icons/senddocument.png" alt="отправить" />
-              {photo ? 'Отправить документ'}
+              <img src="/icons/senddocument.jpg" alt="отправить" />
             </button>
           </>
         ) : (
